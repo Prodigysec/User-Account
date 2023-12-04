7e60097f2ecc5d3eed27d11eacfb72125d4942df
Ian Raburu <williamraburu@gmail.com>
2023-12-04 22:32:08 +0300

    Add an endpoint for deleting a user account

--- server.js
+++ server.js
@@ -38,11 +38,25 @@ app.get('/user/:id', async(req, res) => {
 
 app.put('/user/:id', async(req, res) => {
     try {
+        const {id} = req.params;
         const user = await User.findByIdAndUpdate(id, req.body, {new: true});
         if (!user) {
             return res.status(404).json({message: `cannot find user with id ${id}`})
         }
         res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+app.delete('/user/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const deleted = await User.findByIdAndDelete(id);
+        if (deleted) {
+            return res.status(200).json({message: `user with id ${id} deleted`});
+        }
+        res.status(404).json({message: `user with id ${id} not found`});
     } catch (error) {
         res.status(500).json({error: error.message});
     }
