1ac01f4131863561e4ab18c55033453d1cd5cf33
Ian Raburu <williamraburu@gmail.com>
2023-12-04 16:35:13 +0300

    Connect app to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -6,6 +7,12 @@ app.get('/', (req, res) => {
     res.send('Welcome to User Account App');
 });
 
-app.listen(3000, () => {
-    console.log(`User account app is running on port 3000`)
+mongoose.connect('mongodb://127.0.0.1:27017/useraccount')
+.then(() => {
+    app.listen(3000, () => {
+        console.log(`User account app is running on port 3000`)
+    });
 })
+.catch((err) => {
+    console.log('Failed to connect to MongoDB', err);
+});
