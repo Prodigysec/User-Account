98aa2e8b0c794ef89eb5bea223cfe163ae54cafb
Ian Raburu <williamraburu@gmail.com>
2023-12-05 12:13:00 +0300

    Implement cors

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const User = require('./models/userModel');
+const cors = require('cors');
 const userRoute = require('./routes/userRoute');
 
 const app = express();
 app.use(express.json());
+
+// Middleware for CORS
+app.use(cors()); // Allow all origins with defatult of CORS(*)
+
+// Allow specific origins
+// app.use(
+//     cors({
+//         origin: 'http://localhost:5173',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//         credentials: true
+//     })
+// )
 
 app.get('/', (req, res) => {
     res.send('Welcome to User Account App');
