9dceb08a47aaa93dfbc034d8ece02ded6a630d5b
Ian Raburu <williamraburu@gmail.com>
2023-12-05 11:55:17 +0300

    Add middleware for userRoute

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models/userModel');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 app.use(express.json());
@@ -9,58 +10,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to User Account App');
 });
 
-app.post('/user/register', async(req, res) => {
-    try {
-        const user = await User.create(req.body);
-        res.status(201).json(user);
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
-});
-
-app.get('/users', async(req, res) => {
-    try {
-        const users = await User.find({})
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
-});
-
-app.get('/user/:id', async(req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
-});
-
-app.put('/user/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const user = await User.findByIdAndUpdate(id, req.body, {new: true});
-        if (!user) {
-            return res.status(404).json({message: `cannot find user with id ${id}`})
-        }
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
-});
-
-app.delete('/user/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const deleted = await User.findByIdAndDelete(id);
-        if (deleted) {
-            return res.status(200).json({message: `user with id ${id} deleted`});
-        }
-        res.status(404).json({message: `user with id ${id} not found`});
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
-});
+app.use('/users', userRoute);
 
 mongoose.connect('mongodb://127.0.0.1:27017/useraccount')
 .then(() => {
