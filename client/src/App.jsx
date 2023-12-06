7aedab51540e470b1db78e852264ec208f1a8188
Ian Raburu <williamraburu@gmail.com>
2023-12-06 13:36:16 +0300

    Add routes to different components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
-import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import CreateUser from './pages/CreateUser'
+import DeleteUser from './pages/DeleteUser'
+import Home from './pages/home'
+import ShowUser from './pages/showUser'
+import UpdateUser from './pages/updateUser'
 
 function App() {
-
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<home />} />
+      <Route path="/users/create" element={<CreateUser/>} />
+      <Route path="/users/details/:id" element={<ShowUser/>} />
+      <Route path="/users/edit/:id" element={<UpdateUser/>} />
+      <Route path="/users/delete/:id" element={<DeleteUser/>} />
+    </Routes>
   )
 }
 
