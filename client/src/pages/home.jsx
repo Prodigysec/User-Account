db23e5bb67ae5dab3e0ef15e9eb072e64f1fd8aa
Ian Raburu <williamraburu@gmail.com>
2023-12-06 14:13:58 +0300

    handle form data using usestate

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,23 @@
-import { React } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 function Home() {
+  // handle form data
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: '',
+    address: '',
+    phoneNumber: '',
+    role: '',
+    active: false
+  });
 
+  
   return (
     <div>
       <h1>Home</h1>
