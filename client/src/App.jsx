ce2f65241e9ff484b2817f016a17031f3273e8fa
Ian Raburu <williamraburu@gmail.com>
2023-12-11 13:01:53 +0300

    Add route to login endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import DeleteUser from './pages/DeleteUser'
 import Home from './pages/home'
 import ShowUser from './pages/showUser'
 import UpdateUser from './pages/updateUser'
+import LoginUser from './pages/loginUser'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<home />} />
+      <Route path="/" element={<Home/>} />
+      <Route path='/login' element={<LoginUser/>} />
       <Route path="/users/create" element={<CreateUser/>} />
       <Route path="/users/details/:id" element={<ShowUser/>} />
       <Route path="/users/edit/:id" element={<UpdateUser/>} />
       <Route path="/users/delete/:id" element={<DeleteUser/>} />
     </Routes>
-  )
+ )
 }
 
 export default App
